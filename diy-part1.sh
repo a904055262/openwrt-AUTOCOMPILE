eab6be500e297e3e4354f1541d664891cf69352f
a904055262 <a313852096@gmail.com>
2021-03-10 15:59:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,19 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+git clone  https://github.com/jerrykuku/lua-maxminddb package/0/lua-maxminddb
+git clone  https://github.com/kongfl888/luci-app-adguardhome package/0/luci-app-adguardhome
+git clone  https://github.com/jerrykuku/luci-app-argon-config package/0/luci-app-argon-config
+git clone  https://github.com/garypang13/luci-app-eqos package/0/luci-app-eqos
+git clone  https://github.com/tty228/luci-app-serverchan package/0/luci-app-serverchan
+git clone  https://github.com/pymumu/luci-app-smartdns/ package/0/luci-app-smartdns
+git clone  https://github.com/jerrykuku/luci-app-vssr package/0/luci-app-vssr
+git clone  https://github.com/jerrykuku/luci-theme-argon.git package/0/luci-theme-argon
+git clone  https://github.com/destan19/OpenAppFilter package/0/OpenAppFilter
+git clone  https://github.com/project-openwrt/openwrt-gowebdav.git package/0/openwrt-gowebdav
+git clone  https://github.com/pymumu/openwrt-smartdns package/0/openwrt-smartdns
+svn co  https://github.com/Lienol/openwrt-package/trunk/luci-app-control-timewol package/0/luci-app-control-timewol
+svn co  https://github.com/Lienol/openwrt-package/trunk/luci-app-control-weburl package/0/luci-app-control-weburl
+svn co  https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser package/0/luci-app-filebrowser
+svn co  https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/0/luci-app-openclash
