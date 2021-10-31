0be3e525952882a145daf5e2cb397d7cc17e3c5d
a904055262 <a313852096@gmail.com>
2021-11-01 00:25:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,19 +16,9 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-git clone  https://github.com/jerrykuku/lua-maxminddb package/0/lua-maxminddb
 git clone  https://github.com/kongfl888/luci-app-adguardhome package/0/luci-app-adguardhome
-git clone  https://github.com/jerrykuku/luci-app-argon-config package/0/luci-app-argon-config
-git clone  https://github.com/garypang13/luci-app-eqos package/0/luci-app-eqos
-git clone  https://github.com/tty228/luci-app-serverchan package/0/luci-app-serverchan
-git clone  https://github.com/jerrykuku/luci-app-vssr package/0/luci-app-vssr
-git clone  https://github.com/jerrykuku/luci-theme-argon.git package/0/luci-theme-argon
-rm -rf package/lean/luci-theme-argon
-#git clone  https://github.com/destan19/OpenAppFilter
-git clone  https://github.com/project-openwrt/openwrt-gowebdav.git package/0/openwrt-gowebdav
-svn co  https://github.com/Lienol/openwrt-package/trunk/luci-app-control-timewol package/0/luci-app-control-timewol
-svn co  https://github.com/Lienol/openwrt-package/trunk/luci-app-control-weburl package/0/luci-app-control-weburl
-svn co  https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser package/0/luci-app-filebrowser
+git clone  https://github.com/immortalwrt-collections/openwrt-gowebdav package/0/openwrt-gowebdav
 svn co  https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/0/luci-app-openclash
+git clone --depth=1 https://github.com/fw876/helloworld.git package/0/helloworld
+git clone https://github.com/xiaorouji/openwrt-passwall package/0/passwall
 
-sed -i "s/LUCI_DEPENDS:=@arm/LUCI_DEPENDS:=/g" package/lean/luci-app-cpufreq/Makefile
