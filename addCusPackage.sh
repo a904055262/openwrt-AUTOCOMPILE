1ccdb9e795de3a2223f77a950ba1ad36b01f82a5
a904055262 <a313852096@gmail.com>
2022-12-14 14:51:30 +0800

    Update addCusPackage.sh

--- addCusPackage.sh
+++ addCusPackage.sh
@@ -5,3 +5,5 @@ svn co  https://github.com/kenzok8/openwrt-packages/trunk/smartdns package/0/sma
 svn co  https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns package/0/luci-app-smartdns
 git clone --depth=1 https://github.com/fw876/helloworld.git package/0/helloworld
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/0/luci-app-amlogic
+svn co https://github.com/immortalwrt/packages/trunk/net/msd_lite package/0/msd_lite
+svn co https://github.com/kenzok8/jell/trunk/luci-app-msd_lite package/0/luci-app-msd_lite
