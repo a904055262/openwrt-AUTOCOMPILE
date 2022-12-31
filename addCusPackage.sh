9a788b9804b24af9a74a49a848f84fbd1355eac6
a904055262 <a313852096@gmail.com>
2022-12-31 15:42:46 +0800

    Update addCusPackage.sh

--- addCusPackage.sh
+++ addCusPackage.sh
@@ -2,5 +2,6 @@
 git clone  https://github.com/kongfl888/luci-app-adguardhome package/0/luci-app-adguardhome
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/0/luci-app-openclash
 git clone -b lede  https://github.com/pymumu/luci-app-smartdns package/0/luci-app-smartdns
+git clone https://github.com/pymumu/openwrt-smartdns.git package/0/openwrt-smartdns
 git clone --depth=1 https://github.com/fw876/helloworld.git package/0/helloworld
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/0/luci-app-amlogic
