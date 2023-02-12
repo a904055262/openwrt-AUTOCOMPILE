840b3df8e706e728412c98d95961e34a5df1dcdb
a904055262 <a313852096@gmail.com>
2023-02-12 15:16:17 +0800

    Update delori.sh

--- delori.sh
+++ delori.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
 echo 删除原来的包
-#find package/feeds/ | grep smartdns  | xargs rm -rf
-#find package/feeds/ | grep mosdns  | xargs rm -rf
-find ./package/feeds/ -name 'smartdns'  -type l -exec rm -rf {} \;
-find ./package/feeds/ -name 'luci-app-mosdns'  -type l -exec rm -rf {} \;
-find ./package/feeds/ -name 'mosdns'  -type l -exec rm -rf {} \;
