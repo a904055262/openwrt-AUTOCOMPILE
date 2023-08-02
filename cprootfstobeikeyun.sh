9c2ce172cc1ce02e4752ece268097c63cff84d94
nlf <904055262@qq.com>
2023-08-02 23:43:45 +0800

    beikeyun

---
+++ cprootfstobeikeyun.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+echo 复制rootfs到/opt/openwrt_packit目录
+rootfs=bin/targets/armvirt/64/openwrt-armvirt-64-default-rootfs.tar.gz
+if [ ! -e $rootfs ];then
+	rootfs=bin/targets/armsr/armv8/openwrt-armsr-armv8-generic-rootfs.tar.gz
+fi
+
+if [ ! -e $rootfs ];then
+	echo 不存在arm-rootfs文件
+	exit 1
+fi
+
+if sudo cp -u -v  $rootfs /opt/openwrt_packit/openwrt-armvirt-64-default-rootfs.tar.gz;then
+	echo 复制成功
+else
+	echo 复制失败
+fi
