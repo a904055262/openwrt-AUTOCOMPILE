9ca4c35dc5543c96332b2a17272f1adacbf0c401
a904055262 <a313852096@gmail.com>
2023-10-12 00:27:50 +0800

    localbeikeyunpre.sh

---
+++ localbeikeyunpre.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+sdir=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)
+cd "$sdir"
+
+sudo mkdir -p /opt
+if [ ! -d /opt/openwrt_packit ];then
+	sudo git clone --depth=1 https://github.com/a904055262/openwrt_packit /opt/openwrt_packit
+	if [ $? -ne 0 ];then
+		echo clone失败
+		exit 1
+	fi
+else
+	(
+		cd /opt/openwrt_packit
+		sudo git pull
+	)
+fi
+
+
+sudo cp -vrf beikeyun-kernel/* /opt/kernel/
+sudo cp -vrf beikeyun/* /opt/openwrt_packit/
+
+echo OK
