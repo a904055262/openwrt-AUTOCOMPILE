8530c2ebf0b066902f280f557d38bdbd9a6faf14
a904055262 <a313852096@gmail.com>
2023-10-12 20:42:57 +0800

    beikeyun fw3

--- fullcone3-patch.sh
+++ fullcone3-patch.sh
@@ -18,7 +18,7 @@ cd "$sdir"
 declare -A dlAddr
 dlAddr=(
 [firewall]=https://github.com/immortalwrt/immortalwrt/branches/openwrt-23.05/package/network/config/firewall
-[luci-app-firewall]=https://github.com/immortalwrt/luci/branches/openwrt-23.05/applications/luci-app-firewall
+#[luci-app-firewall]=https://github.com/immortalwrt/luci/branches/openwrt-23.05/applications/luci-app-firewall
 [fullcone]=https://github.com/immortalwrt/immortalwrt/branches/openwrt-23.05/package/network/utils/fullconenat
 )
 
@@ -54,6 +54,48 @@ done
 cd ..
 
 
+patchlucifirewall(){
+	# 给luci-app-firewall 打补丁
+	echo 开始给 luci-app-firewall 打补丁
+
+	local zonejs=feeds/luci/applications/luci-app-firewall/htdocs/luci-static/resources/view/firewall/zones.js
+	if [ ! -f $zonejs ];then
+		echo 找不到zonejs
+		exit 1
+	fi
+
+	if grep -q 'Full-cone' $zonejs;then
+		echo 已经打过了
+		return 0
+	fi
+
+	cp $zonejs $zonejs.bak 
+
+	local tmf=$(mktemp)
+	cat <<-'EOF' > $tmf
+		o = s.option(form.Flag, 'fullcone', _('Full-cone NAT'));
+	EOF
+
+	#cat $tmf
+
+	local linenum=$(sed -n '/drop_invalid/{
+			=;q
+		}' $zonejs)
+	if [ ! "$linenum" ];then
+		echo 找不到修改点
+		exit 1
+	fi
+
+	sed -i "$linenum r $tmf
+		" $zonejs
+
+	echo luci-app-firewall 打补丁-成功
+	
+}
+
+patchlucifirewall
+
+
 #exit
 
 echo '开始替换源文件'
@@ -63,7 +105,7 @@ declare -A rPath
 rPath=(
 [firewall]='../package/network/config/'
 [fullcone]='../package/'
-[luci-app-firewall]='../feeds/luci/applications/'
+#[luci-app-firewall]='../feeds/luci/applications/'
 )
 
 for d in $(ls);do
