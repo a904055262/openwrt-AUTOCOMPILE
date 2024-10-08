89f50aec003c587e620d06ebbabd452a21aacab9
a904055262 <a313852096@gmail.com>
2024-10-08 17:24:54 +0800

    u

---
+++ addcompiletime.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+sdir=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)
+cd "$sdir"
+
+f=feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+
+t="Compiled: $(TZ='Asia/Shanghai' date '+%x %T')"
+
+sed -i -E '/Firmware Version/{
+	s~,\s*$~ + "'"/$t"'" ,~
+}' "$f"
+
+sed -n '/Firmware Version/p' "$f"
